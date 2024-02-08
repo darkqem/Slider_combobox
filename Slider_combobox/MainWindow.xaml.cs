24262e3b792c4ec59a6619f619976df34755c8b4
God <God@DESKTOP-PS27JDE>
2024-02-09 01:02:45 +0500

    Добавьте файлы проекта.

---
+++ Slider_combobox/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Slider_combobox
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Slider_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var slider = sender as Slider;
+            double increment;
+            if (slider != null)
+            {
+                if (e.Delta > 0)
+                {
+                    increment = 1;
+                }
+                else
+                {
+                    increment = -1;
+                }
+                slider.Value += increment;
+                e.Handled = true; 
+            }
+        }
+    }
+}
